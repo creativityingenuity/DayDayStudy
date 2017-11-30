d3b40b2d732ef6bbcbaf5c2f32b0af5875afbb36
yangtuo <vipggxs@163.com>
2017-11-30 17:10:34 +0800

    mvp demo init

--- app/src/main/java/com/yt/daydaystudy/MainActivity.java
+++ app/src/main/java/com/yt/daydaystudy/MainActivity.java
@@ -1,5 +1,7 @@
 package com.yt.daydaystudy;
 
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test1();
+    }
+
+    /**
+     * lrucache diskLrucache
+     */
+    private void test1() {
+        /*设置LruCache缓存的大小，一般为当前进程可用容量的1/8*/
+        int maxMemoCache = (int) (Runtime.getRuntime().totalMemory() / 1024/8);
+        LruCache lruCache = new LruCache<String,Bitmap>(maxMemoCache){
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                /*重写sizeOf方法，计算出要缓存的每张图片的大小*/
+                return value.getRowBytes()*value.getHeight()/1024;
+            }
+        };
+
     }
 }
---
+++ mvpdemo/src/main/java/yt/mvpdemo/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package yt.mvpdemo.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yt.mvpdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
