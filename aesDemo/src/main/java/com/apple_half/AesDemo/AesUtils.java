f79d198235adce67210c730f452a7e69aeeb73e1
yangtuo <vipggxs@163.com>
2017-12-09 11:58:27 +0800

    12  9 aes rsa加密demo 完成

---
+++ aesDemo/src/main/java/com/apple_half/AesDemo/AesUtils.java
@@ -0,0 +1,96 @@
+package com.apple_half.AesDemo;
+
+import android.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.Charset;
+import java.security.Key;
+import java.security.Security;
+
+/**
+ * Created by apple_half on 15-1-20.
+ *
+ */
+public class AesUtils {
+
+    /**
+     * 密钥算法
+     */
+    private static final String KEY_ALGORITHM = "AES";
+
+    /**
+     * AES/CBC/PKCS7Padding 分别对应
+     * 加密||解密算法、工作模式、填充方式
+     */
+    private static final String CIPHER_ALGORITHM = "AES/CBC/PKCS7Padding";
+
+
+    private static byte[] getKey() throws Exception {
+        return MainActivity.Encryption_SecretKey.getBytes(Charset.forName("UTF-8"));
+    }
+
+    /**
+     *
+     * 加密数据, 返回字节数组
+     *
+     */
+    public static byte[] encrypt1(String toEncrypt) throws Exception {
+        Key secretKey = new SecretKeySpec(getKey(), KEY_ALGORITHM);
+
+        // libs 中 bcprov 的支持, bouncycastle 支持 64 位密钥
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+
+        // 获取位移，并初始化
+        final byte[] ivData = MainActivity.Encryption_IV.getBytes();
+        IvParameterSpec iv = new IvParameterSpec(ivData);
+
+        // 用 iv 初始化
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+
+        final byte[] encryptedMessage = cipher.doFinal(toEncrypt.getBytes(Charset.forName("UTF-8")));
+
+        final byte[] ivAndEncryptedMessage = new byte[ivData.length + encryptedMessage.length];
+        System.arraycopy(ivData, 0, ivAndEncryptedMessage, 0, ivData.length);
+        System.arraycopy(encryptedMessage, 0, ivAndEncryptedMessage, ivData.length, encryptedMessage.length);
+
+        return ivAndEncryptedMessage;
+    }
+
+    /**
+     *
+     * 加密数据，返回 String 对象
+     *
+     */
+    public static String encrypt2(String toEncrypt) throws Exception {
+        byte[] ivAndEncryptedMessage = encrypt1(toEncrypt);
+        String strResult = new String(Base64.encode(ivAndEncryptedMessage, 0), "UTF-8");
+        return strResult;
+    }
+
+    /*
+     *
+     * 解密数据
+     *
+     */
+    public static String decrypt(String toDecrypt) throws Exception {
+        byte[] data = Base64.decode(toDecrypt, 0);
+
+        Key secretKey = new SecretKeySpec(getKey(), KEY_ALGORITHM);
+
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+        final byte[] ivData = MainActivity.Encryption_IV.getBytes();
+        IvParameterSpec iv = new IvParameterSpec(ivData);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        final byte[] encryptedMessage = cipher.doFinal(data);
+
+        final byte[] result = new byte[encryptedMessage.length - ivData.length];
+        System.arraycopy(encryptedMessage, ivData.length, result, 0, result.length);
+        return new String(result);
+    }
+
+}
---
+++ aesDemo/src/main/java/com/apple_half/AesDemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.apple_half.AesDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // 定义自己的秘钥、位移量
+    public final static String Encryption_SecretKey = "A9e4/vnQTrKF6otAGbM6zGsulKEL7b3x";
+    public final static String Encryption_IV = "9mg+!7ed8b36*w`X";
+
+    // 提供要加密的数据
+    private String data = "android 加密解密测试";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initView();
+    }
+
+    private void initView() {
+        TextView secreTv = (TextView) findViewById(R.id.secret_key);
+        secreTv.setText("SecretKey: " + Encryption_SecretKey);
+
+        TextView ivTv = (TextView) findViewById(R.id.iv);
+        ivTv.setText("iv: " + Encryption_IV);
+
+        final EditText dataTv = (EditText) findViewById(R.id.data);
+        dataTv.setText(data);
+
+        final TextView result1 = (TextView) findViewById(R.id.result1);
+        final TextView result2 = (TextView) findViewById(R.id.result2);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setText("加密");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String encrypted = AesUtils.encrypt2(dataTv.getText().toString());
+                    result1.setText(encrypted);
+
+                    // 解密
+                    String decrypted = AesUtils.decrypt(encrypted);
+                    result2.setText("对加密后的结果，进行解密：\n" + decrypted);
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+}
