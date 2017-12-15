48b2f83cab3bfe916225f8fbde52e570cc6fc0dc
gxs <vipgxs@gmail.com>
2017-12-15 14:05:52 +0800

    你xx 很洗脑啊

--- mvpdemo/src/main/java/yt/mvpdemo/base/BasePresenter.java
+++ mvpdemo/src/main/java/yt/mvpdemo/base/BasePresenter.java
@@ -4,7 +4,7 @@ package yt.mvpdemo.base;
  * Created by ${zhangyuanchao} on 2017/11/30.
  */
 
-public interface BasePresenter<T,E> {
+public abstract class BasePresenter<T,E> {
 //    public Context mContext;
 //    public E mModel;
 //    public T mView;
