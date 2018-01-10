1f18ad55da9cfedc80aaed93d988300c58420946
gxs <vipgxs@gmail.com>
2018-01-10 13:02:16 +0800

    表盘view练习 over

--- app/src/main/java/com/yt/daydaystudy/test_diyview/BiaoPanView.java
+++ app/src/main/java/com/yt/daydaystudy/test_diyview/BiaoPanView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -18,6 +19,8 @@ public class BiaoPanView extends View {
     private Paint circlePaint;
     private Paint textPaint;
     private Paint keduPaint;
+    private Path textPath;
+    private Path keduPath;
 
     public BiaoPanView(Context context) {
         this(context, null);
@@ -36,17 +39,40 @@ public class BiaoPanView extends View {
         circlePaint = new Paint();
         circlePaint.setColor(Color.RED);
         circlePaint.setStyle(Paint.Style.STROKE);
+        circlePaint.setDither(true);
+
         textPaint = new Paint();
         textPaint.setColor(Color.BLUE);
         textPaint.setStyle(Paint.Style.STROKE);
+        textPaint.setStrokeWidth(1);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextSize(15);
+
         keduPaint = new Paint();
         keduPaint.setColor(Color.BLUE);
         keduPaint.setStyle(Paint.Style.STROKE);
+
+        textPath = new Path();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(0,0,10,circlePaint);
+        //移动画布坐标到150 150
+        canvas.translate(150,150);
+        //画半径为100的圆
+        canvas.drawCircle(0,0,100,circlePaint);
+        //沿着某条Path来绘制这些文字
+        textPath.moveTo(-100,0);
+        textPath.lineTo(0,-100);
+        canvas.drawTextOnPath("灵魂只能独行",textPath,20,0,textPaint);
+
+        canvas.save();
+        for (int i = 1;i<=12;i++) {
+            canvas.rotate(30);
+            canvas.drawLine(0,-100,0,-80,keduPaint);
+        }
+        canvas.restore();
     }
+
 }
