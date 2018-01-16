f314186941a83fc07a099c651bd169f9deb47e8c
gxs <vipgxs@gmail.com>
2018-01-16 11:50:47 +0800

    修改fragment案例错误

--- demo_oneactivity_multifragment/src/main/java/com/example/demo_oneactivity_multifragment/ForgetpwdFragment1.java
+++ demo_oneactivity_multifragment/src/main/java/com/example/demo_oneactivity_multifragment/ForgetpwdFragment1.java
@@ -1,25 +1,8 @@
 package com.example.demo_oneactivity_multifragment;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.blmf.R;
-import com.blmf.mvp.contract.ChangePhoneContract;
-import com.blmf.mvp.model.ChangePhoneModel;
-import com.blmf.mvp.model.entity.RegisterEntity;
-import com.blmf.mvp.presenter.ChangePhonePresenter;
-import com.blmf.mvp.ui.activity.ForgetActivity;
-
-import blmf.lib_commen.base.BaseFragment;
-import blmf.lib_commen.utils.CountDownTimerUtils;
-import blmf.lib_commen.utils.ToastUtils;
-import butterknife.Bind;
-import butterknife.OnClick;
 
 /**
  * Created by ${zhangyuanchao} on 2017/12/18.
@@ -50,7 +33,7 @@ public class ForgetpwdFragment1 extends BaseFragment {
 
     public void startNextFragment(View v) {
         if (mActivity instanceof onFragmentItemClickListener) {
-            ((MainActivity) mActivity).goNext("123456789");
+//            ((MainActivity) mActivity).goNext("123456789");
         }
     }
 
