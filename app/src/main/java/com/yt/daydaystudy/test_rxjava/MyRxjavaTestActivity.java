8691f3270a42e5269522472becbe3c57eac97522
gxs <vipgxs@gmail.com>
2018-07-13 10:45:12 +0800

    ARouter test

---
+++ app/src/main/java/com/yt/daydaystudy/test_rxjava/MyRxjavaTestActivity.java
@@ -0,0 +1,18 @@
+package com.yt.daydaystudy.test_rxjava;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Call:vipggxs@163.com
+ * Created by YT on 2018/7/12.
+ */
+
+public class MyRxjavaTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ demo_uifix/src/main/java/com/example/demo_uifix/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.demo_uifix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://juejin.im/post/5ae9cc3a5188253dc612842b
+ * https://juejin.im/post/5b3094fc6fb9a00e52398ae4#heading-0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
