37654aae4b8505364b04e47c92c12cecc40d18a9
gxs <vipgxs@gmail.com>
2018-08-20 09:13:45 +0800

    转换Excel 工具初步集成

---
+++ demo_export_excel/src/main/java/cn/yt/demo_export_excel/Student.java
@@ -0,0 +1,28 @@
+package cn.yt.demo_export_excel;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+
+public class Student {
+
+    String name;
+    String sex;
+    String age;
+    String id;
+    String classNo;
+    String math;
+    String english;
+    String chinese;
+
+    public Student(String name, String sex, String age, String id, String classNo, String math, String english, String chinese) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.id = id;
+        this.classNo = classNo;
+        this.math = math;
+        this.english = english;
+        this.chinese = chinese;
+    }
+}
