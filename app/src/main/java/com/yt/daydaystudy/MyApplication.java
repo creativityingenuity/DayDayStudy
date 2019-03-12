9eca08bda97289822c9549e83fdc0d6c0735d9c1
imooc <vipgxs@gmail.com>
2019-03-12 21:12:41 +0800

    删除重复代码

--- app/src/main/java/com/yt/daydaystudy/MyApplication.java
+++ app/src/main/java/com/yt/daydaystudy/MyApplication.java
@@ -20,7 +20,7 @@ public class MyApplication extends Application {
             ARouter.openLog();     // 打印日志
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
-        //初始化Arouter
+        //初始化Arouter  临时添加
         ARouter.init(this);
     }
 
